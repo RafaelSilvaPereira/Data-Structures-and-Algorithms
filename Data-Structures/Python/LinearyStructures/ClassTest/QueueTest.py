9bee78081bf3feafcbc4cbc9bb9813f7f2229d7c
RafaelSilvaPereira <rafael.pereira@ccc.ufcg.edu.br>
2018-11-01 10:23:15 -0300

    adding queue

---
+++ Data-Structures/Python/LinearyStructures/ClassTest/QueueTest.py
@@ -0,0 +1,28 @@
+import unittest
+from LinearyStructures.Queue import Queue
+
+class QueueTest(unittest.TestCase):
+
+    def setUp(self):
+        self._queue = Queue()
+        self._otherQueue = Queue()
+
+        for i in range(9):
+            self._queue.add(i)
+
+    def testIsEmpty(self):
+        self.assertTrue( self._otherQueue.isEmpty )
+
+    def testPeek(self):
+        self.assertEqual(0,self._queue.peek())
+        self.assertIsNone(self._otherQueue.peek())
+
+    def testPoll(self):
+        self.assertEqual(0, self._queue.poll())
+        self.assertEqual(1, self._queue.peek())
+
+    def testAddInQueue(self):
+        self._otherQueue.add(33)
+        self.assertEqual(33, self._otherQueue.peek())
+
+
